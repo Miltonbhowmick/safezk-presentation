d733eaae1544708fb2fa06ead891230e2b793a19
Miltonbhowmick <miltonbhowmick6@gmail.com>
2023-03-15 23:30:13 +0600

    Added all sections with little bit mobile responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleMobileNavbar(){
+    var x = document.getElementById('mobileMenus');
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
